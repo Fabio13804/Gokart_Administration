9d2dd13150f406bfaab7fed4fcff4c172f599848
Fabio Notarfrancesco <notarfrancescof@bzz.ch>
2022-05-19 08:18:53 +0200

    Added model class

---
+++ src/main/java/ch/bzz/GokartAdministration/model/Circuit.java
@@ -0,0 +1,4 @@
+package ch.bzz.GokartAdministration.model;
+
+public class Circuit {
+}
---
+++ src/main/java/ch/bzz/GokartAdministration/model/Karting_company.java
@@ -0,0 +1,11 @@
+package ch.bzz.GokartAdministration.model;
+
+import java.util.List;
+
+public class Karting_company {
+    private int karting_companyID;
+    private List<Gokart> gokartList;
+    private String name;
+    private boolean restaurant;
+
+}
